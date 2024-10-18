db25feb98774bcb3894d1aa166fabf33f784bad1
Your Name <fpasamuelmayowa51@gmail.com>
2024-10-18 15:41:17 +0100

    profile fix

--- js/load.js
+++ js/load.js
@@ -5,18 +5,11 @@
         mailChimpURL: ''   // mailchimp url
     };
     const $WIN = $(window);
-
-    // Add the User Agent to the <html>
-    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
-    const doc = document.documentElement;
-    doc.setAttribute('data-useragent', navigator.userAgent);
-
     /* Preloader
     * -------------------------------------------------- */
-
     const ssPreloader = function () {
-        $WIN.addClass('ss-preload');
-        $(window).on('load', function () {
+        $("html").addClass('ss-preload');
+        $WIN.on('load', function () {
             // will first fade out the loading animation 
             $("#loader").fadeOut("slow", function () {
                 // will fade out the whole DIV that covers the website.
